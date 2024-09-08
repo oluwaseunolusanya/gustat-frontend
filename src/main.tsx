cb2cd6c77f7b6b0c089bab93f561cf88f3ca82d3
Oluwaseun Olusanya <oluwaseun_olusanya@yahoo.com>
2024-09-08 16:43:38 +0100

    Added routes to the main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter as Router } from "react-router-dom";
+import AppRoutes from './AppRoutes';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Router></Router>
+    <Router>
+      <AppRoutes />
+    </Router>
   </StrictMode>,
 )
