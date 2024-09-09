91cfd8cbc90294a2aa74414360877acf5ca089d8
Oluwaseun Olusanya <oluwaseun_olusanya@yahoo.com>
2024-09-09 20:30:41 +0100

    Changed hero image and created Home page component.

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,14 @@
+const HomePage = () => {
+  return (
+    <div className="flex flex-col gap-12">
+        <div className="bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
+            <h1 className="text-5xl font-bold tracking-tight text-red-800">
+                Tuck into tasty delights
+            </h1>
+            <span className="text-xl">Just a click away!</span>
+        </div>
+    </div>
+  );
+};
+
+export default HomePage;
