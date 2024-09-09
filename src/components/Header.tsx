91bea5ea45836882d982a87c51423f3bb29bd236
Oluwaseun Olusanya <oluwaseun_olusanya@yahoo.com>
2024-09-09 11:56:13 +0100

    Added the mobile nav component to the header.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import MobileNav from "./MobileNav";
 
 export default function Header() {
   return (
@@ -10,6 +11,9 @@ export default function Header() {
             >
                 Gustat
             </Link>
+            <div>
+              <MobileNav />
+            </div>
         </div>
     </div>
   )
