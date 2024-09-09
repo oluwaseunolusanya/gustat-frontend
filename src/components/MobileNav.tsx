3d471228ca65c055c2cb10529f9df9e950a7b3d9
Oluwaseun Olusanya <oluwaseun_olusanya@yahoo.com>
2024-09-09 11:54:04 +0100

    Setup the sheet component for navigation.

---
+++ src/components/MobileNav.tsx
@@ -0,0 +1,25 @@
+import { Menu } from "lucide-react";
+import { Sheet, SheetContent, SheetTrigger, SheetTitle, SheetDescription } from "./ui/sheet";
+import React from "react";
+import { Separator } from "./ui/separator";
+import { Button } from "./ui/button";
+
+export default function MobileNav() {
+  return (
+    <Sheet>
+        <SheetTrigger>
+            <Menu className="text-red-500" />
+        </SheetTrigger>
+        <SheetContent>
+            <SheetTitle>
+                <span> Welcome to Gustat!</span>
+            </SheetTitle>
+            <Separator />
+            <SheetDescription className="flex">
+                <Button className="flex-1 font-bold bg-red-500">Log in</Button>
+            </SheetDescription>
+        </SheetContent>
+
+    </Sheet>
+  )
+}
