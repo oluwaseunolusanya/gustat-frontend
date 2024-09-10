fade73d7e373b8a62088244584fda32b4d553eb9
Oluwaseun Olusanya <oluwaseun_olusanya@yahoo.com>
2024-09-10 20:13:52 +0100

    Added auth0 redirection to the login button.

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -1,10 +1,15 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "./ui/button";
 
 const MainNav = () => {
+  // Create a variable to hook into auth0 provider.
+  const { loginWithRedirect } = useAuth0();
+
   return (
     <Button 
         variant="ghost" 
         className="font-bold hover:text-red-800 hover:bg-white"
+        onClick={ async () => await loginWithRedirect() } 
     >
         Log in
     </Button>
