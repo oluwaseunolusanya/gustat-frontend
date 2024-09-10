83f1baae1a55b4cabb25bd68096c2c6f98fdfa22
Oluwaseun Olusanya <oluwaseun_olusanya@yahoo.com>
2024-09-10 19:26:52 +0100

    Updated the content of the footer.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,7 @@
 const Footer = () => {
   return (
     <div className="bg-red-800 py-10">
-        <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
-            <span className="text-3xl text-white font-bold tracking-tight">
-                Gustat
-            </span>
+        <div className="container mx-auto flex flex-col md:flex-row justify-center items-center">
             <span className="text-white font-bold tracking-tight flex gap-4">
                 <span>Copyright © 2024 Gustat</span>
             </span>
