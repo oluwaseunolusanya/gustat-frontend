bcda3988d17c865581f1063213a3a6d9fd46cc4e
Oluwaseun Olusanya <oluwaseun_olusanya@yahoo.com>
2024-09-11 18:32:26 +0100

    Added logged in user menu.

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -1,5 +1,6 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "./ui/button";
+import UsernameMenu from "./UsernameMenu";
 
 const MainNav = () => {
   // Create a variable to hook into auth0 provider.
