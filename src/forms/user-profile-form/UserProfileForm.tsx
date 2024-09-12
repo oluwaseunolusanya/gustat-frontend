0d803553756356bddd97e4892c62c472e4fe26e2
Oluwaseun Olusanya <oluwaseun_olusanya@yahoo.com>
2024-09-12 20:55:12 +0100

    Defined form schema.

---
+++ src/forms/user-profile-form/UserProfileForm.tsx
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+const formSchema = z.object({
+    email: z.string().optional(),
+    name: z.string().min(1, "name is required"),
+    addressLine1: z.string().min(1, "Address Line 1 is required"),
+    city: z.string().min(1, "City is required"),
+    Country: z.string().min(1, "Country is required"),
+})
