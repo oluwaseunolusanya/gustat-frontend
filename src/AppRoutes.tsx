eb1b0c0e6b9005cd56d765aa5b398ea3c6114d4b
Oluwaseun Olusanya <oluwaseun_olusanya@yahoo.com>
2024-09-12 16:14:03 +0100

    Routed the auth call back page.

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,6 +1,7 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./layouts/layout";
 import HomePage from "./pages/HomePage";
+import AuthCallbackPage from "./pages/AuthCallbackPage";
 
 const AppRoutes = () => {
     return(
@@ -10,7 +11,9 @@ const AppRoutes = () => {
                 element={
                     <Layout>
                         <HomePage />
-                    </Layout>} />
+                    </Layout>}
+            />
+            <Route path="/auth-callback" element={<AuthCallbackPage />} />
             <Route path="/user-profile" element={<span>USER PROFILE PAGE</span>} />
             <Route path="*" element={<Navigate to="/" />} />
         </Routes>
