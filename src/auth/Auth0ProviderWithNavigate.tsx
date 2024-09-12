77ce4d716da2596fc0b5dd86d1f9ca718357ad72
Oluwaseun Olusanya <oluwaseun_olusanya@yahoo.com>
2024-09-12 15:50:54 +0100

    Called create user hook.

--- src/auth/Auth0ProviderWithNavigate.tsx
+++ src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
+import { useCreateMyUser } from "@/api/MyUserApi";
 
 // Define the Props type, indicating that this component accepts children
 type Props = {
@@ -8,6 +9,8 @@ type Props = {
 
 // Main component definition
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
+    const { createUser,  } = useCreateMyUser();
+
     // Retrieve environment variables for Auth0 configuration
     const domain = import.meta.env.VITE_AUTH0_DOMAIN;
     const clientId = import.meta.env.VITE_AUTH0_CLIENTID;
@@ -21,7 +24,10 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
     // Callback function to handle what happens after Auth0 redirects back to the app post-login
     const onRedirectCallback = (appState?: AppState, user?: User) => {
         // Log the user information (if available) to the console for debugging or further processing
-        console.log("USER", user);
+        // console.log("USER", user);
+        if(user?.sub && user?.email){
+            createUser({auth0Id: user.sub, email: user.email});
+        }
     };
 
     // Render the Auth0Provider component, passing the domain, clientId, and redirect URI
