57acd97ae211cb66a2d1c99c2706034a82c328e2
Oluwaseun Olusanya <oluwaseun_olusanya@yahoo.com>
2024-09-12 15:18:17 +0100

    Created custom hook for api request to create user.

--- src/api/MyUserApi.tsx
+++ src/api/MyUserApi.tsx
@@ -1,1 +1,38 @@
+import { useMutation } from "react-query";
+
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+type CreateUserRequest = {
+    auth0Id: String;
+    email: string;
+};
+
+export const useCreateMyUser = () => {
+    const createMyUserRequest = async (User: CreateUserRequest) => {
+        const response = await fetch(`${API_BASE_URL}/api/my/user`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application.json",
+            },
+            body: JSON.stringify(user),
+        });
+
+        if(!response.ok){
+            throw new Error("Failed to create user request");
+        }
+    };
+
+    const { 
+        mutateAsync: createUser, 
+        isLoading, 
+        isError, 
+        isSuccess,
+     } = useMutation(createMyUserRequest);
+
+     return{
+        createUser,
+        isLoading,
+        isError,
+        isSuccess,
+     };
+};
