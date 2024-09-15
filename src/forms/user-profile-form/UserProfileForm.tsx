1d7d6c8b6e179df140d8edff0b72c5937fb1c6e0
Oluwaseun Olusanya <oluwaseun_olusanya@yahoo.com>
2024-09-15 08:20:09 +0100

    Added form fields to create user profile.

--- src/forms/user-profile-form/UserProfileForm.tsx
+++ src/forms/user-profile-form/UserProfileForm.tsx
@@ -1,7 +1,8 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 import { useForm } from "react-hook-form";
-import { Form } from "@/components/ui/form";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 const formSchema = z.object({
     email: z.string().optional(),
@@ -25,8 +26,81 @@ const UserProfileForm = ({ onSave, isLoading }:props) => {
 
     return (
         <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSave)} className="space-y-4 bg-gray-50 rounded-lg md:p-10">
+            <form 
+                onSubmit={form.handleSubmit(onSave)} 
+                className="space-y-4 bg-gray-50 rounded-lg md:p-10"
+            >
+                <div>
+                    <h2 className="text-2xl font-bold">User Profile Form</h2>
+                    <FormDescription>
+                        View and change your profile information here
+                    </FormDescription>
+                </div>
+                <FormField 
+                    control={form.control} 
+                    name="email" 
+                    render={({field}) => (
+                        <FormItem>
+                            <FormLabel>Email</FormLabel>
+                            <FormControl>
+                                <Input {...field} disabled className="bg-white" />
+                            </FormControl>
+                        </FormItem>
+                    )} 
+                />
+                <FormField 
+                    control={form.control} 
+                    name="name" 
+                    render={({field}) => (
+                        <FormItem>
+                            <FormLabel>Name</FormLabel>
+                            <FormControl>
+                                <Input {...field} disabled className="bg-white" />
+                            </FormControl>
+                        </FormItem>
+                    )}
+                />
 
+                <div className="flex flex-col md:flex-row gap-4">
+                    <FormField 
+                        control={form.control} 
+                        name="addressLine1" 
+                        render={({field}) => (
+                            <FormItem>
+                                <FormLabel>Address Line 1</FormLabel>
+                                <FormControl>
+                                    <Input {...field} disabled className="bg-white" />
+                                </FormControl>
+                            </FormItem>
+                        )} 
+                    />
+
+                    <FormField 
+                        control={form.control} 
+                        name="city" 
+                        render={({field}) => (
+                            <FormItem>
+                                <FormLabel>City</FormLabel>
+                                <FormControl>
+                                    <Input {...field} disabled className="bg-white" />
+                                </FormControl>
+                            </FormItem>
+                        )} 
+                    />
+
+                    <FormField 
+                        control={form.control} 
+                        name="country" 
+                        render={({field}) => (
+                            <FormItem>
+                                <FormLabel>Country</FormLabel>
+                                <FormControl>
+                                    <Input {...field} disabled className="bg-white" />
+                                </FormControl>
+                            </FormItem>
+                        )} 
+                    />
+                </div>
             </form>
         </Form>
     )
