1ba0c6cfa4dae394732aac0678ffb14cc9916b31
Oluwaseun Olusanya <oluwaseun_olusanya@yahoo.com>
2024-09-27 18:46:28 +0100

    Added aspect ratio for images

--- src/forms/manage-restaurant-form/ImageSection.tsx
+++ src/forms/manage-restaurant-form/ImageSection.tsx
@@ -1,9 +1,12 @@
+import { AspectRatio } from "@/components/ui/aspect-ratio";
 import { FormControl, FormDescription, FormField, FormItem, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { useFormContext } from "react-hook-form";
 
 const ImageSection = () => {
-    const { control } = useFormContext();
+    const { control, watch } = useFormContext();
+
+    const existingImageUrl = watch("imageUrl");
 
     return (
         <div className="space-y-2">
@@ -15,6 +18,14 @@ const ImageSection = () => {
             </div>
 
             <div className="flex flex-col gap-8 w-[50%]">
+                {existingImageUrl && (
+                    <AspectRatio ratio={16/9}>
+                        <img 
+                            src={existingImageUrl} 
+                            className="rounde-md object-cover h-full w-full"
+                        />
+                    </AspectRatio>
+                )}
                 <FormField
                     control = {control}
                     name="imageFile"
